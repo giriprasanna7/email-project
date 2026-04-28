1b3021b17fe579b0cb7836ab474bfd849d0e9f3e
Giriprasanna K <160580315+giriprasanna7@users.noreply.github.com>
2026-04-28 16:27:26 +0530

    Update ALLOWED_HOSTS to allow all hosts

--- email_project/settings.py
+++ email_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["34.208.181.79"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
